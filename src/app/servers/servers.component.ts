f45a2a199edfd4566394620ede7863777c069c74
Rohan Thorat <rthorat1@binghamton.edu>
2017-09-25 11:24:47 -0400

    bootstrap and angular colors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server> 
-    <p>We are trying Inline template in component. It works fine.</p>
-    <app-server></app-server> `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
